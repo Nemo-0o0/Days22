8a62a104e814669611a0162b9e3ae4a7756a8b3e
das-110722 <phuocvu.hcmc@gmail.com>
2024-06-22 03:41:17 +0700

    Daysss

---
+++ app.js
@@ -0,0 +1,47 @@
+const yourDate = new Date("2024-01-22T18:00:00"),
+music = ['one', 'two', 'three'];
+document.addEventListener('DOMContentLoaded', function(){
+      var rootTime = document.querySelector("time");
+
+      document.querySelector("anni").textContent = `${(yourDate.getDate()>9)?yourDate.getDate():"0"+yourDate.getDate()}-${(yourDate.getMonth()>8)?(yourDate.getMonth()+1):"0"+(yourDate.getMonth()+1)}-${yourDate.getFullYear()}`;
+      
+      document.querySelector("date").textContent = Math.floor( Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24)+" DAYS";
+
+      function olock() {
+            var today = new Date(),
+            hrs = (Math.floor( Math.floor((today - yourDate) / 1000) / 60 / 60)) % 24,
+            min = (Math.floor( Math.floor((today - yourDate) / 1000) / 60)) % 60,
+            sec =  Math.floor((today - yourDate) / 1000) % 60;
+            rootTime.textContent = `${(hrs>9)?hrs:"0"+hrs}:${(min>9)?min:"0"+min}:${(sec>9)?sec:"0"+sec}`;
+      } olock();
+      var timer = setInterval(function(){olock()}, 1000);
+      document.querySelector("audio").setAttribute("src", `music/${music[Math.floor(Math.random()*music.length)]}.mp3`);
+
+}, false);
+
+// modal
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("openModalBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
